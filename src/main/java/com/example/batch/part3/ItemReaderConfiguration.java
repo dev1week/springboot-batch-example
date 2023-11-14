328036e60f394962f3f136cb929d5a6680b2429d
dev1week <rondo2860@gmail.com>
2023-11-15 08:42:11 +0900

    feat: jpa에서 읽기(#4)

--- src/main/java/com/example/batch/part3/ItemReaderConfiguration.java
+++ src/main/java/com/example/batch/part3/ItemReaderConfiguration.java
@@ -8,7 +8,9 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.JpaCursorItemReader;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
@@ -17,6 +19,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,10 +35,13 @@ public class ItemReaderConfiguration {
 
     private final DataSource dataSource;
 
-    public ItemReaderConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, DataSource dataSource) {
+    private  final EntityManagerFactory entityManagerFactory;
+
+    public ItemReaderConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, DataSource dataSource, EntityManagerFactory entityManagerFactory) {
         this.jobBuilderFactory = jobBuilderFactory;
         this.stepBuilderFactory = stepBuilderFactory;
         this.dataSource = dataSource;
+        this.entityManagerFactory = entityManagerFactory;
     }
 
     @Bean
@@ -46,6 +52,7 @@ public class ItemReaderConfiguration {
                 //csv 파일을 읽는 step을 job에 추가함
                 .next(this.csvFileStep())
                 .next(this.jdbcStep())
+//                .next(this.jpaStep())
                 .build();
     }
 
@@ -138,6 +145,28 @@ public class ItemReaderConfiguration {
         return stepBuilderFactory.get("jdbcStep")
                 .<Person, Person>chunk(10)
                 .reader(jdbcCursorItemReader())
+                .writer(itemWriter())
+                .build();
+    }
+
+    //JPA에서 읽어오기
+    private JpaCursorItemReader<Person> jpaCursorItemReader() throws Exception {
+        JpaCursorItemReader<Person> itemReader =  new JpaCursorItemReaderBuilder<Person>()
+                .name("jpaCursorItemReader")
+                .entityManagerFactory(entityManagerFactory)
+                .queryString("select p from Person p")
+                .build();
+
+        itemReader.afterPropertiesSet();
+
+        return itemReader;
+    }
+
+    @Bean
+    public Step jpaStep() throws Exception {
+        return stepBuilderFactory.get("jpaStep")
+                .<Person, Person>chunk(10)
+                .reader(this.jpaCursorItemReader())
                 .writer(itemWriter())
                 .build();
     }
--- src/main/java/com/example/batch/part3/Person.java
+++ src/main/java/com/example/batch/part3/Person.java
@@ -1,15 +1,27 @@
 package com.example.batch.part3;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
 @Getter
 public class Person {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String age;
     private String address;
 
+    public Person(String name, String age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
 
     public Person(int id, String name, String age, String address) {
         this.id = id;
